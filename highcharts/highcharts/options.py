49a6be96f49f0268a9cc70aef120f9a98f46bdda
Jeremy Brunn <contactme@jeremybrunn.com>
2021-05-24 11:07:02 -0400

    Update options.py

--- highcharts/highcharts/options.py
+++ highcharts/highcharts/options.py
@@ -255,7 +255,7 @@ class ExportingOptions(BaseOptions):
         "type": basestring,
         "url": basestring,
         "width": int,
-        "showTable": False
+        "showTable": bool
     }
 
 
